96bac95d28a056d5cdc812ae6e4856c5ec4b7846
limongi1234 <rntlemoslimongideaguiarmoraes@gmail.com>
2025-02-15 02:29:48 -0300

    Criação da lógica no app.js

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const amigos = [];
+const listaAmigos = document.querySelector("#listaAmigos");
+const resultado = document.getElementById('resultado');
+const amigo = document.querySelector('#amigo');
+const btnAdd = document.querySelector(".button-add");
+const btnSort = document.querySelector(".button-draw");
+
+const adicionarAmigo = () => {
+  const valor = amigo.value;
+
+  if(valor.trim() !== "") {
+    amigos.push(valor);
+    const arrayAmigos = amigos.map((item, index) => `<li class="amigo" key=${index}>${item}</li>`).join('');
+    listaAmigos.innerHTML = arrayAmigos;
+
+    alert(`Seu amigo ${valor} adicionado com sucesso`);
+    amigo.value = "";
+  }
+}
+
+function sortearAmigo() {
+  const index = Math.floor(Math.random() * amigos.length);
+  const amigoSorteado = amigos[index];
+  resultado.innerHTML = `<li class="amigo_sorteado">O amigo(a) sorteado foi ${amigoSorteado}</li>`;
+}
+
+btnAdd.addEventListener("click", adicionarAmigo);
+btnSort.addEventListener("click", sortearAmigo);
+
+btnAdd.addEventListener("click", adicionarAmigo);
+btnSort.addEventListener("click", sortearAmigo);
